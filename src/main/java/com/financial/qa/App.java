8dcf8889ed884ae858c0da6c5d5a177f40076194
oreoanytime <adrianustw.2021@scis.smu.edu.sg>
2025-05-27 05:29:15 +0800

    Initial commit: Restructure project, add JUnit 5 setup and basic test

---
+++ src/main/java/com/financial/qa/App.java
@@ -0,0 +1,17 @@
+package com.financial.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+}
